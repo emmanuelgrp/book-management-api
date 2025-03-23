d2764f78063a75b0abd7f6b34765c5d7205c9708
Javier Emmanuel Granados Pardo <javieremmanuelgranados@outlook.com>
2025-03-23 15:51:44 -0700

    first commit

---
+++ src/test/java/com/codemainlabs/book_management_api/BookManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemainlabs.book_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
