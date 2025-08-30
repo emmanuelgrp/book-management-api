fa2da91d18feef6fe19eae90a6b8fd03b18a259d
Javier Emmanuel Granados Pardo <javieremmanuelgranados@outlook.com>
2025-08-29 20:00:05 -0700

    HelloWorldController Test added2

--- src/main/java/com/codemainlabs/book_management_api/controller/HelloWorldController.java
+++ src/main/java/com/codemainlabs/book_management_api/controller/HelloWorldController.java
@@ -1,4 +1,14 @@
 package com.codemainlabs.book_management_api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+    @GetMapping("/helloworld")
+    public String helloWorld() {
+        return "Hello World";
+    }
 }
+
